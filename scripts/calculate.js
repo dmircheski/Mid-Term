585d6ebaa5bab54de6900ba3d5aacedac769af14
dmircheski <deni.mirceski@t.mk>
2020-10-22 18:36:45 +0200

    event listener added

--- scripts/calculate.js
+++ scripts/calculate.js
@@ -2,8 +2,30 @@ let num1 = document.getElementById('n1');
 let num2 = document.getElementById('n2');
 let checkbox = document.getElementById("agreeCheckbox");
 
-calculate = () => {
-    if (areBothInputsPopulated()) {
+document.getElementById('btn').addEventListener("click", calculate);
+
+function checkField1() {
+    if(num1.value.length == 0) {
+        document.getElementById('error1').innerHTML = "Cannot leave number 1 empty";
+        return true;
+    } else {
+        document.getElementById('error1').innerHTML = "";
+        return false;
+    }
+}
+
+function checkField2() {
+    if(num1.value.length == 0) {
+        document.getElementById('error2').innerHTML = "Cannot leave number 2 empty";
+        return true;
+    } else {
+        document.getElementById('error2').innerHTML = "";
+        return false;
+    }
+}
+
+function calculate() {
+    if (checkField1() && checkField2() == true) {
         if (checkbox.checked) {
             let result = Number(num1.value) + Number(num2.value);
             localStorage.setItem("Result", result);
@@ -11,12 +33,7 @@ calculate = () => {
         } else {
             alert("You need to check the box");
         }
-    } else {
-        alert("You must populate both fields");
-    }
+    } 
 
 }
 
-areBothInputsPopulated = () => {
-    return num1.value.length && num2.value.length
-}
